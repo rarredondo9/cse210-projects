dfea932df108229a616f125d1b068cb8919bcb56
rarredondo9 <rebeca.arredondo.9@gmail.com>
2023-10-26 21:46:10 +0100

    Learning ACTIVITY

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+         Assignment assignmentOne = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignmentOne.GetSummary());
+
+
+        MathAssignment assignmentTwo = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assignmentTwo.GetSummary());
+        Console.WriteLine(assignmentTwo.GetHomeworkList());
+
+        WritingAssignment assignmentThree = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignmentThree.GetSummary());
+        Console.WriteLine(assignmentThree.GetWritingInformation());
     }
+
 }
