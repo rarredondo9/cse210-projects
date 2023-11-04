61e51b888dc6a367ca4de2482c59963e64659c29
rarredondo9 <rebeca.arredondo.9@gmail.com>
2023-11-04 13:55:17 +0000

    messages and perform activity

--- prove/Develop04/ActivityClass.cs
+++ prove/Develop04/ActivityClass.cs
@@ -1,6 +1,6 @@
-using System
+using System;
 
-public class ActivityClass
+public abstract class ActivityClass
 {
     protected string name;
     protected string description;
@@ -12,15 +12,30 @@ public class ActivityClass
         this.description = description;
     }
 
-    public void SetDuration (int duration)
-    {
-        this.duration = duration;
-    }
-
-    public void StarActivity()
+    public void Start()
     {
         DisplayStartingMessage();
         PerformActivity();
         DisplayEndingMessage();
     }
+
+    protected void DisplayStartingMessage()
+    {
+        Console.WriteLine($"Starting {name} Activity:");
+        Console.WriteLine(description);
+        Console.WriteLine($"Duration: {duration} seconds");
+        Console.WriteLine("Prepare to begin...");
+        System.Threading.Thread.Sleep(2000);
+    }
+
+    protected void DisplayEndingMessage()
+    {
+        Console.WriteLine($"Great Job! You have completed the {name} Activity.");
+        Console.WriteLine($"Duration: {duration} seconds");
+        System.Threading.Thread.Sleep(2000);
+        Console.WriteLine("Activity Completed.");
+        System.Threading.Thread.Sleep(2000);
+    }
+
+    protected abstract void PerformActivity();
 }
